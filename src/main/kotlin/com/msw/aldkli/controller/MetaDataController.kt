c52ae31c193c2a0cae3de968f813c7525ed48f50
xjh <qqq000qqq@aliyun.com>
2023-08-25 00:58:28 +0800

    20230825

--- src/main/kotlin/com/msw/aldkli/controller/MetaDataController.kt
+++ src/main/kotlin/com/msw/aldkli/controller/MetaDataController.kt
@@ -3,14 +3,15 @@ package com.msw.aldkli.controller
 import com.msw.aldkli.meta.ApiGroupMetaData
 import com.msw.aldkli.scanner.ApiScanner
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/aldkli/metaData")
+@RequestMapping("aldkli/metaData")
 class MetaDataController {
 
-    @GetMapping("getApiGroupMetaDataList")
-    fun getApiGroupMetaDataList(scanPackage: String): List<ApiGroupMetaData> = ApiScanner().scan(scanPackage)
+    @GetMapping("getApiGroupMetaDataList/{scanPackage}")
+    fun getApiGroupMetaDataList(@PathVariable scanPackage: String): List<ApiGroupMetaData> = ApiScanner().scan(scanPackage)
 
 }
